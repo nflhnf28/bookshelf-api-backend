1332414cc7a150227f17610b733a81b387dd73c5
nflhnf28 <nfl.hnf28@gmail.com>
2024-05-24 20:27:28 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// Memuat kode untuk membuat, mengonfigurasi,
+// dan menjalankan server HTTP menggunakan Hapi.
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+    // Allow CORS - in Global all routes
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
